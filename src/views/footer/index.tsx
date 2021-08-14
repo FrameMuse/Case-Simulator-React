e74df513ceef77bb1d2d57dab21fb27687b61aa0
Valery <minicablestone@gmail.com>
2021-08-14 04:11:43 +0300

    remove footer desc

--- src/views/footer/index.tsx
+++ src/views/footer/index.tsx
@@ -6,16 +6,10 @@
 // SCSS
 import "../../assets/scss/views/footer.scss"
 // IMAGES
-import visaImage from "../../assets/images/pay_systems/visa.png"
-import mastercardImage from "../../assets/images/pay_systems/mastercard.png"
-import qiwiImage from "../../assets/images/pay_systems/qiwi.png"
-import yandexImage from "../../assets/images/pay_systems/yandex.png"
-import webmoneyImage from "../../assets/images/pay_systems/webmoney.png"
 // STAFF
 import GoUp from "./GoUp"
 import { StandoffLogo } from "app/components/UI/Logo"
 import SocialNets from "./SocialNets"
-import { Link } from "react-router-dom"
 import useTranslation from "resources/hooks/useTranslation"
 
 export default function Footer() {
@@ -38,29 +32,6 @@ export default function Footer() {
           </div>
         </div>
         <SocialNets />
-      </section>
-      <section>
-        <p className="footer__desc">
-          {footer.desc}
-        </p>
-      </section>
-      <section>
-        <div className="footer-links">
-          <Link className="footer-links__link" to="/terms-contacts">{footer.links?.termsContracts}</Link>
-        </div>
-        <div className="footer-logos">
-          <img src={visaImage} alt="pay system logo" className="footer-logos__logo" />
-          <img src={mastercardImage} alt="pay system logo" className="footer-logos__logo" />
-          <img src={qiwiImage} alt="pay system logo" className="footer-logos__logo" />
-          <img src={yandexImage} alt="pay system logo" className="footer-logos__logo" />
-          <img src={webmoneyImage} alt="pay system logo" className="footer-logos__logo" />
-          <a href="//freekassa.ru/">
-            <img src="//www.free-kassa.ru/img/fk_btn/17.png" title="Приём оплаты на сайте картами" />
-          </a>
-          <a href="https://freekassa.ru" target="_blank" rel="noopener noreferrer">
-            <img src="https://cdn.freekassa.ru/banners/small-dark-1.png" title="Прием платежей" />
-          </a>
-        </div>
       </section>
       <section>
         <p className="footer__desc">
