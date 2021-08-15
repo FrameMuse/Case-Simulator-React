d192a6d77a4a4fa26e8557e2e5bb671b703cc46d
Valery <minicablestone@gmail.com>
2021-08-15 03:33:22 +0300

    Add local database

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-namespace */
 import Weapons from "./data/weapons.json"
+import Cases from "./data/cases.json"
 
 namespace DataBase {
-  export const data = { Weapons }
+  export const data = { Weapons, Cases }
 }
 
 export default DataBase
